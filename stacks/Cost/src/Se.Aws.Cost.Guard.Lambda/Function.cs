c93ab625545e1bf0565e4d11d728287168da1a4d
Федчук Ігор Васильович <i.fedchuk@temabit.com>
2025-08-24 12:55:14 +0300

    stage commig

--- stacks/Cost/src/Se.Aws.Cost.Guard.Lambda/Function.cs
+++ stacks/Cost/src/Se.Aws.Cost.Guard.Lambda/Function.cs
@@ -28,7 +28,7 @@ public sealed class Function
     }
 
     // Entry point
-#pragma warning disable IDE0060 // Remove unused parameter
+#pragma warning disable IDE0060 // Remove unused parameter exclusion
     public async Task Handler(object? input, ILambdaContext context)
 #pragma warning restore IDE0060
     {
